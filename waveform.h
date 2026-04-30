a044cdf30b3d57d4211516458e049782ab309b22
KevalKurd <KEVALMUSTAFA6@GMAIL.COM>
2026-04-30 23:01:42 +0100

    Define waveform sample structure

--- waveform.h
+++ waveform.h
@@ -4,5 +4,19 @@
 
 #ifndef POWER_QUALITY_ANALYSER_WAVEFORM_H
 #define POWER_QUALITY_ANALYSER_WAVEFORM_H
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
 
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} WaveformSample;
+
+#endif
 #endif //POWER_QUALITY_ANALYSER_WAVEFORM_H
