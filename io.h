6f8e140d4dbc6f1bc61d7b4c3c47ee006d1a2970
KevalKurd <KEVALMUSTAFA6@GMAIL.COM>
2026-05-01 20:23:58 +0100

    Add file input and output function prototypes

--- io.h
+++ io.h
@@ -2,7 +2,18 @@
 // Created by k3-mustafa on 30/04/2026.
 //
 
-#ifndef POWER_QUALITY_ANALYSER_IO_H
-#define POWER_QUALITY_ANALYSER_IO_H
+#ifndef IO_H
+#define IO_H
 
-#endif //POWER_QUALITY_ANALYSER_IO_H
+#include "waveform.h"
+
+// Count number of data rows in CSV
+int count_rows(const char *filename);
+
+// Load CSV into dynamically allocated array
+WaveformSample *load_csv_file(const char *filename, int *count);
+
+// Write analysis results to file
+void write_results_file(const char *filename, WaveformSample *data, int count);
+
+#endif
